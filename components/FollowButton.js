3aee2f5f6d6ae224cf10e6683a4abd992170104b
Alex Villecco <amvillecco@teco.com.ar>
2023-04-08 17:39:33 -0300

    create FollowButton

---
+++ components/FollowButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  TouchableHighlight,
+  View,
+  Text,
+} from 'react-native'
+
+const FollowButton = () => (
+  <TouchableHighlight>
+    <View>
+      <Text>follow</Text>
+    </View>
+  </TouchableHighlight>
+)
+
+export default FollowButton
